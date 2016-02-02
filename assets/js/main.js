9960f83f993422586a51485d678ca06bc5fd1311
Brandon Mikulka <brandon.mikulka@colorado.edu>
2016-02-02 09:27:11 -0700

    add navbar colapse on menu click and style a bit for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,48 +43,17 @@ function handleTopNavAnimation() {
 }
 
 /*
- * Registration Form
-*/
-
-$('#registration-form').submit(function(e){
-    e.preventDefault();
-
-    var postForm = { //Fetch form data
-            'fname'     : $('#registration-form #fname').val(),
-            'lname'     : $('#registration-form #lname').val(),
-            'email'     : $('#registration-form #email').val(),
-            'cell'      : $('#registration-form #cell').val(),
-            'address'   : $('#registration-form #address').val(),
-            'zip'       : $('#registration-form #zip').val(),
-            'city'      : $('#registration-form #city').val(),
-            'program'   : $('#registration-form #program').val()
-    };
-
-    $.ajax({
-            type      : 'POST',
-            url       : './assets/php/contact.php',
-            data      : postForm,
-            dataType  : 'json',
-            success   : function(data) {
-                            if (data.success) {
-                                $('#registration-msg .alert').html("Registration Successful");
-                                $('#registration-msg .alert').removeClass("alert-danger");
-                                $('#registration-msg .alert').addClass("alert-success");
-                                $('#registration-msg').show();
-                            }
-                            else
-                            {
-                                $('#registration-msg .alert').html("Registration Failed");
-                                $('#registration-msg .alert').removeClass("alert-success");
-                                $('#registration-msg .alert').addClass("alert-danger");
-                                $('#registration-msg').show();
-                            }
-                        }
-        });
-});
-
-/*
  * SmoothScroll
 */
 
 smoothScroll.init();
+
+/*
+ * Colapse menu on click
+*/
+
+$(document).ready(function(){
+    $(".navbar-collapse li").on("click", function(){
+        $(".navbar-collapse").collapse("hide");
+    });
+});
